4e11079af707575059610dbd3e4a99675e83d0a8
gholzemer <gh0026@mix.wvu.edu>
2023-03-13 12:18:36 -0400

    Init

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+    try {
+        $serverName = "wvurms.database.windows.net";
+        $databaseName = "RMS APP";
+        $uid = "azureuser";
+        $pwd = "WVUrms12";
+        
+        $conn = new PDO("sqlsrv:server = $serverName; Database = $databaseName;", $uid, $pwd);
+    
+        // Select Query
+        $tsql = "SELECT @@Version AS SQL_VERSION";
+    
+        // Executes the query
+        $stmt = $conn->query($tsql);
+    } catch (PDOException $exception1) {
+        echo "<h1>Caught PDO exception:</h1>";
+        echo $exception1->getMessage() . PHP_EOL;
+        echo "<h1>PHP Info for troubleshooting</h1>";
+    }
+    
+?>    
