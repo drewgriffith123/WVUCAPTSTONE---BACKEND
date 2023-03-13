c7519c906a6a802ed69279abe66a073572a86cca
gholzemer <gh0026@mix.wvu.edu>
2023-03-13 13:27:52 -0400

    test

--- config.php
+++ config.php
@@ -1,18 +1,6 @@
 <?php
 
-/* Handle CORS */
 
-// Specify domains from which requests are allowed
-header('Access-Control-Allow-Origin: *');
-
-// Specify which request methods are allowed
-header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
-
-// Additional headers which may be sent along with the CORS request
-header('Access-Control-Allow-Headers: X-Requested-With,Authorization,Content-Type');
-
-// Set the age to 1 day to improve speed/caching.
-header('Access-Control-Max-Age: 86400');
 
 try {
     $serverName = "wvurms.database.windows.net";
