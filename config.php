18f66c7a316b95247b9ed57def7b5534461fab52
gholzemer <gh0026@mix.wvu.edu>
2023-03-13 18:53:57 -0400

    init config

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:wvurms.database.windows.net,1433; Database = RMS APP", "azureuser", "WVUrms12");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+    // PHP Data Objects(PDO) Sample Code:
+    try {
+        $conn = new PDO("sqlsrv:server = tcp:wvurms.database.windows.net,1433; Database = RMS APP", "azureuser", "WVUrms12");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $e) {
+        echo "Error connecting to SQL Server.";
+        die(print_r($e));
+    }
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "azureuser", "pwd" => "{your_password_here}", "Database" => "RMS APP", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:wvurms.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+    // SQL Server Extension Sample Code:
+    $connectionInfo = array("UID" => "azureuser", "pwd" => "{your_password_here}", "Database" => "RMS APP", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $serverName = "tcp:wvurms.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
   
