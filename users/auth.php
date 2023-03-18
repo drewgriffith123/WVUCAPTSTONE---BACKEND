d0fc4b46f3a6a159fcff7e02f2e30e2b50bfa585
gholzemer <gh0026@mix.wvu.edu>
2023-03-18 12:30:57 -0400

    init

--- users/auth.php
+++ users/auth.php
@@ -2,19 +2,25 @@
     // https://learn.microsoft.com/en-us/sql/connect/php/sqlsrv-get-field?view=sql-server-ver16
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Methods: GET");
+    header("Access-Control-Allow-Methods: GET, POST");
     // header("Access-Control-Max-Age: 3600");
     // header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     include '../config.php';
     $database = new database();
     $db = $database->getConnection();
 
-    if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['action'] === 'hello') {
-        hello();
+    if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['action'] === 'login') {
+        login();
     }
 
-    function hello(){
-        echo 'hello';
+    function login(){
+
+        $name = $_GET['name'];
+        echo "Hello, $name!";
+        
+        $password = $_GET['password'];
+        echo "Password, $password!";
+        
     }
 
     function hi(){
